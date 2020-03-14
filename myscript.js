10db22387ff33940a1d7f4878bd8d2f53b6b038c
andy <andylouisjean@gmail.com>
2020-03-14 02:18:57 -0400

    show my love hw

---
+++ myscript.js
@@ -0,0 +1,15 @@
+document.getElementById("button-press");
+
+function sayHello (){
+    const input = "What is your name?";
+    const input2 = "What is your favorite color?";
+}
+
+const color = (color: "blue", "red", "black");
+
+if (input2 !== "blue", "red", "black") {
+    alert("The color you have chosen is incompatible");
+}
+else if(input2 ===  "blue", "red", "black" ){
+    alert("Yay that is my favorite color too");
+}
